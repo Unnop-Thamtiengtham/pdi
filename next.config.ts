d27daa8af27842f103c1e8d80506a6a1169a1e2d
BM-Unnop <122899012+Unnop-Thamtiengtham@users.noreply.github.com>
2026-07-31 15:19:13 +0700

    fix: upadate next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -39,9 +39,9 @@ const nextConfig: NextConfig = {
           value: [
             "default-src 'self'",
             "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-            "style-src 'self' 'unsafe-inline'",
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "img-src 'self' data: blob: https://*.digitaloceanspaces.com",
-            "font-src 'self'",
+            "font-src 'self' https://fonts.gstatic.com",
             "connect-src 'self' data: blob: https://*.digitaloceanspaces.com",
             "frame-ancestors 'none'",
           ].join('; '),
